da5eb8a4b9a3c9da2dfd52014fc4290228610baf
samk97 <sandeepsingh150797@gmail.com>
2023-04-19 12:51:17 +0530

    redirection

--- frontend/src/components/QuestionsSection.js
+++ frontend/src/components/QuestionsSection.js
@@ -1,6 +1,8 @@
 import QuestionArea from "./QuestionArea";
 
 function QuestionsSection(props) {
+
+
   return (
     <>
       {/* Questions */}
@@ -17,16 +19,6 @@ function QuestionsSection(props) {
           op_c="Lorem C"
           op_d="Lorem D"
           ans="B"
-          exp="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        ></QuestionArea>
-
-        <QuestionArea
-          question="This is a dummy question 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          op_a="Lorem A This is a dummy question 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
-          op_b="Lorem B"
-          op_c="Lorem C"
-          op_d="Lorem D"
-          ans="C"
           exp="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         ></QuestionArea>
       </div>
