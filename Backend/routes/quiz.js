aaa78ef97da722f5686a395f16d6b2892fcd3a79
samk97 <sandeepsingh150797@gmail.com>
2023-04-29 00:51:21 +0530

    changes quiz details

--- Backend/routes/quiz.js
+++ Backend/routes/quiz.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { authCheck, profileCheck,loginCheck,tokenVerifier } = require("../middlewares/auth");
 const {
-fetchLeaderBoard
+fetchLeaderBoard,fetchLeaderBoardById
 } = require("../controller/quiz");
 
 
 
 //Route to Fetch LeaderBoard
 router.post("/get-leaderboard",fetchLeaderBoard);
+
+
+router.post("/get-leaderboard-by-id",fetchLeaderBoardById);
 
 
 
