39caae9ae40af5ddcb6f34fa20b129ba6fc918be
piyush <piyushjaiswal956@gmail.com>
2023-06-05 04:07:32 +0530

    Revert change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";nn
 import "./index.css";
 import App from "./App";
 import {createStore} from 'redux';
