d2e379241b4d2c0daae04b1396a1431f7975ea53
piyush <piyushjaiswal956@gmail.com>
2023-06-05 04:19:20 +0530

    All Service Worker removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";nn
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from "./reducers";
-import swDev from './swDev';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const store = createStore(rootReducer,composeWithDevTools());
@@ -17,8 +17,6 @@ root.render(
     </BrowserRouter>
     </Provider>
 );
-swDev();
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- frontend/src/swDev.js
+++
@@ -1,6 +0,0 @@
-export default function swDev(){
-    let swUrl = `${process.env.REACT_APP_FPURL}/sw.js`
-    navigator.serviceWorker.register(swUrl).then((res)=>{
-        console.log("response",res);
-    })
-}
